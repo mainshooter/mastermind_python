df136892aa5207685969a94cde116ba182d1df22
Peter Romijn <peter@samebestdevelopment.nl>
2020-03-15 13:55:43 +0100

    Can calculate code

--- app.py
+++ app.py
@@ -6,7 +6,21 @@ app = Flask(__name__)
 app.debug = True
 app.secret_key = b'\x17\xc7#\x94\xfa&\xd5\xff\xec\x9f1\xb5\xe4\nv\xf7'
 
+mastermind = Mastermind()
+
+def startUp():
+    if 'name' in session:
+        mastermind.playerName = session['playerName']
+        mastermind.started = True
+
 @app.route('/')
 def hello_world():
-    var_dump(Mastermind)
     return render_template('welcome.html');
+
+@app.route('/playername', methods=['POST'])
+def playerName():
+    mastermind.playerName = request.form['playerName']
+    mastermind.doubleColors = False
+    mastermind.generate()
+    mastermind.save()
+    return render_template('welcome.html')
--- src/mastermind.py
+++ src/mastermind.py
@@ -1,6 +1,32 @@
+from flask import session
+import random
+from var_dump import var_dump
+
 class Mastermind:
 
     def __init__(self):
-        self.playername = ""
+        self.playerName = ""
         self.started = False
+        self.doubleColors = False
         pass
+
+    def generate(self):
+        solutionColors = []
+        for x in range(4):
+            foundColor = False
+            while foundColor is False:
+                posibleColor = random.randint(1, 5)
+                for color in solutionColors:
+                    if (posibleColor == color):
+                        foundColor = False
+                        break
+                    else:
+                        foundColor = True
+                if foundColor is True or len(solutionColors) == 0:
+                    solutionColors.append(posibleColor)
+        var_dump(solutionColors)
+
+
+    def save(self):
+        session['playerName'] = self.playerName
+        session['doubleColors'] = self.doubleColors
